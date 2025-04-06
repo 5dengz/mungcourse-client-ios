ae82528a99f2d490d4aff3058222092c9d41dd8a
Kyoungho Eom <kheom0512@gmail.com>
2025-04-06 23:12:51 +0900

    feat: 신규 Xcode 프로젝트 구성
    프로젝트 구성 파일, 자산, SwiftUI 및 SwiftData 모델, 테스트 템플릿 추가
    앱 데모 기능 및 UI/테스트 초기 셋업

---
+++ mungcourse/mungcourseTests/mungcourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  mungcourseTests.swift
+//  mungcourseTests
+//
+//  Created by Kyoungho Eom on 4/6/25.
+//
+
+import Testing
+@testable import mungcourse
+
+struct mungcourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
