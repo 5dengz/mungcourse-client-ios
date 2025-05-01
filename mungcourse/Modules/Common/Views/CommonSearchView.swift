4506e61eafe516f81fe202d095e1c9168639b763
Kyoungho Eom <kheom0512@gmail.com>
2025-05-01 13:50:38 +0900

    refactor: 검색뷰 및 경유지 뷰 개선
    공통 검색 UI에서 제네릭 trailingContent 제거 및 고정 검색 아이콘 적용
    경유지 선택 뷰에 뒤로가기 동작 구현과 네비게이션 UI 개선

--- mungcourse/Modules/Common/Views/CommonSearchView.swift
+++ mungcourse/Modules/Common/Views/CommonSearchView.swift
@@ -1,25 +1,11 @@
 import SwiftUI
 
 // 가고 싶은 장소를 검색하기 위한 공통 검색 UI 컴포넌트
-struct CommonSearchView<TrailingContent: View>: View {
+struct CommonSearchView: View {
     let placeholder: String
-    let trailingContent: () -> TrailingContent
 
-    init(
-        placeholder: String = "가고 싶은 장소를 검색하세요",
-        @ViewBuilder trailingContent: @escaping () -> TrailingContent
-    ) {
-        self.placeholder = placeholder
-        self.trailingContent = trailingContent
-    }
-}
-
-// 팡당 검색 아이콘 등 우측 컨텐츠가 없을 때 사용
-extension CommonSearchView where TrailingContent == EmptyView {
     init(placeholder: String = "가고 싶은 장소를 검색하세요") {
-        self.init(placeholder: placeholder) {
-            EmptyView()
-        }
+        self.placeholder = placeholder
     }
 }
 
@@ -33,35 +19,27 @@ extension CommonSearchView {
                             .font(Font.custom("Pretendard", size: 14))
                             .lineSpacing(21)
                             .foregroundColor(Color(red: 0.62, green: 0.62, blue: 0.62))
-                        ZStack {
-                            trailingContent()
-                        }
-                        .frame(width: 22, height: 22)
+                        Spacer()
+                        Image("icon_search")
+                            .resizable()
+                            .frame(width: 22, height: 22)
                     }
                 }
             }
             .padding(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
             .background(Color(red: 0.95, green: 0.95, blue: 0.95))
             .cornerRadius(9)
+            .frame(width: 353)
         }
-        .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20))
+        .padding(.horizontal, 20)
     }
 }
 
 #if DEBUG
 struct CommonSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        VStack(spacing: 20) {
-            // 기본 검색 필드
-            CommonSearchView()
-
-            // 검색 아이콘이 있는 검색 필드
-            CommonSearchView {
-                Image(systemName: "magnifyingglass")
-                    .foregroundColor(.gray)
-            }
-        }
-        .previewLayout(.sizeThatFits)
+        CommonSearchView()
+            .previewLayout(.sizeThatFits)
     }
 }
 #endif
--- mungcourse/Modules/Walk/Views/SelectWaypointView.swift
+++ mungcourse/Modules/Walk/Views/SelectWaypointView.swift
@@ -1,12 +1,16 @@
 import SwiftUI
 
 struct SelectWaypointView: View {
+    @Environment(\.dismiss) private var dismiss
+
     var body: some View {
         VStack(spacing: 0) {
             CommonHeaderView(leftIcon: "arrow_back", leftAction: {
-                // TODO: 뒤로 가기 기능 구현
+                dismiss()
             }, title: "경유지 선택")
             CommonSearchView()
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 20)
                 .padding(.vertical, 10)
             // 컨텐츠 영역 placeholder
             ScrollView {
@@ -20,7 +24,7 @@ struct SelectWaypointView: View {
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
-        .ignoresSafeArea(edges: .top)
+        .navigationBarHidden(true)
     }
 }
 
