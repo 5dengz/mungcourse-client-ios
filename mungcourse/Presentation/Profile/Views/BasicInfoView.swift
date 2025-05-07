17e4cc7245a53ad184e4df4e0d1e2bdc0ed8f602
Kyoungho Eom <kheom0512@gmail.com>
2025-05-08 04:33:16 +0900

    refactor: 불필요한 ScrollView 제거
    뷰 구조 단순화를 위해 중복된 ScrollView와 패딩 제거

--- mungcourse/Presentation/Profile/Views/BasicInfoView.swift
+++ mungcourse/Presentation/Profile/Views/BasicInfoView.swift
@@ -5,8 +5,7 @@ struct BasicInfoView: View {
     @EnvironmentObject var dogVM: DogViewModel
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 15) {
+        VStack(spacing: 15) {
             if let detail = dogVM.dogDetail {
                 HStack {
                     Text("견종/성별")
@@ -62,10 +61,8 @@ struct BasicInfoView: View {
                     .frame(maxWidth: .infinity, minHeight: 200)
             }
         }
-            }
-            .font(.custom("Pretendard-Regular", size: 14))
-            .padding()
-            .background(Color("pointwhite"))
-            .padding(.bottom, tabBarHeight)
+        .font(.custom("Pretendard-Regular", size: 14))
+        .padding()
+        .background(Color("pointwhite"))
     }
 }
